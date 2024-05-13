cb44fec1adae1e85ee8fb4300d067ab6cc73b714
Amon De Shir <amonsir29@gmail.com>
2024-05-13 09:47:43 +0200

    create 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,46 +1,28 @@
 import * as React from "react"
 import { Link, HeadFC, PageProps } from "gatsby"
+import { Page } from "../components/page/page"
+import { NavBar } from "../components/nav/nav"
+import { Title } from "../components/title/title";
+import { Text } from "../components/text/text";
+import { Part } from "../components/part/part";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+const pages: [string, string, number][] = [
+  ["KSIĄŻKA", "/#wierszoterapie", 0],
+  ["WIERSZE", "/#poems", 2],
+  ["KONTAKT", "/#contact", 3],
+  ["O MNIE", "/#about-me", 1],
+];
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Page>
+      <NavBar pages={pages} selected="" useLinks />
+      <Part>
+        <Title>404</Title>
+        <Text>Nie odnaleziono strony</Text>
+      </Part>
+    </Page>
   )
 }
 
