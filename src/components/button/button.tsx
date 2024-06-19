39da574fea61e6b7699b5478b26769f8ef5b9d33
Amon De Shir <amonsir29@gmail.com>
2024-06-19 16:06:31 +0200

    implement order form

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -156,3 +156,57 @@ export const Button4 = styled.a`
       transform: scale(0.8);
   }
 `
+
+
+export const SubmitButton = styled.button<{ bigger?: boolean }>`
+  --width: ${({ bigger }) => bigger ? "42.5rem" : "25rem" };
+  cursor: pointer;
+  display: block;
+  position: relative;
+  width: var(--width);
+  height: 8rem;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.secondary};
+  font-family: ${({ theme }) => theme.fonts.ui};
+  margin-top: 10px;
+  margin-left: auto;
+  transition: transform 0.25s, color 0.5s, background-color 0.5s, border-color 0.5s;
+  background: none;
+  border: none;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.secondary};
+    border-color: ${({ theme }) => theme.colors.primary};
+    color: ${({ theme }) => theme.colors.primary};
+    transform: scale(0.9);
+  }
+
+  &::before {
+    content: " ";
+    z-index: -1;
+    position: absolute;
+    background-color: ${({ theme }) => theme.colors.primary};
+    border: 2px solid ${({ theme }) => theme.colors.secondary};
+    width: var(--width);
+    height: 8rem;
+    border-radius: 10rem;
+    transition: transform 0.25s, color 0.5s, background-color 0.5s, border-color 0.5s;
+  }
+  
+  &:hover {
+    color: ${({ theme }) => theme.colors.primary};
+    &::before {
+      background-color: ${({ theme }) => theme.colors.secondary};
+      border-color: ${({ theme }) => theme.colors.primary};
+      transform: scale(0.9);
+    }
+  }
+  
+  &:active {
+      transform: scale(0.8);
+  }
+`
--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -4,6 +4,7 @@ import { Page } from "../components/page/page"
 import { SEO } from "../components/seo/seo";
 import { NavBar } from "../components/nav/nav";
 import styled from "@emotion/styled";
+import { SubmitButton } from "../components/button/button";
 
 const pages: [string, string, number][] = [
   ["KSIĄŻKA", "/#wierszoterapie", 0],
@@ -27,13 +28,101 @@ const Text = styled.div`
   flex-direction: column;
 `;
 
+const Label = styled.label`
+  display: block;
+  font-size: 3.2rem;
+  text-align: left;
+  font-weight: normal;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  display: block;
+  border: 0px;
+  border-bottom: 1px solid black;
+  transition: border-color 0.25s;
+  font-size: 3rem;
+  padding: 1rem;
+  margin-bottom: 5rem;
+  width: 50rem;
+
+  &:hover, &:focus {
+    outline: none;
+    border-color: ${({ theme }) => theme.colors.primary}; 
+  }
+`;
+
+const Area = styled.textarea`
+  display: block;
+  border: 0px;
+  border-bottom: 1px solid black;
+  transition: border-color 0.25s;
+  font-size: 3rem;
+  padding: 1rem;
+  margin-bottom: 5rem;
+  width: 50rem;
+  height: 20rem;
+
+  &:hover, &:focus {
+    outline: none;
+    border-color: ${({ theme }) => theme.colors.primary}; 
+  }
+`;
+
+const Title = styled.div`
+  font-size: 4rem;
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.primary};
+  font-family: ${({ theme }) => theme.fonts.ui};
+  text-align: center;
+  padding-bottom: 1rem;
+  margin-bottom: auto;
+`;
+
+const Center = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Form = styled.form`
+  padding: 10rem;
+`;
+
+const Buttons = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <Page>
         <NavBar selected="" pages={pages} useLinks />
-        <Text>
-          Tu będzie formularz zamówieniowy książki
-        </Text>
+          <Center>
+            <Title>Zamów książkę z dedykacją</Title>
+            <Form method="post" action="/api/wierszoterapie/order">
+              <Label>
+                Twój adres email
+                <Input name="email" type="email" id="email" />
+              </Label>
+
+              <Label>
+                Uwagi
+                <Area name="email" id="email" />
+              </Label>
+
+              <Buttons>
+                <div>
+                  <SubmitButton type="submit">Wyślij</SubmitButton>
+                </div>
+              </Buttons>
+            </Form>
+          </Center>
     </Page>
   )
 }
